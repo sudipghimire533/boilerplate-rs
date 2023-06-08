150d54c35501779f13f1d3845af369693924446d
sudipghimire533 <sudipghimire533@gmail.com>
2023-06-08 13:51:56 +0200

    traits::Get does not take self

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,7 @@
 /// Get something
 pub trait Get<T> {
     /// Get something
-    fn get(&self) -> T;
+    fn get() -> T;
 }
 
 #[cfg(test)]
@@ -9,25 +9,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn wrapper_type() {
-        struct I32Getter(i32);
+    fn get_default_type() {
+        struct MagicNumberGetter;
 
-        impl Get<i32> for I32Getter {
-            fn get(&self) -> i32 {
-                self.0
+        impl Get<i32> for MagicNumberGetter {
+            fn get() -> i32 {
+                42
             }
         }
 
-        impl Get<i64> for I32Getter {
-            fn get(&self) -> i64 {
-                self.0.into()
-            }
-        }
-
-        let getter = I32Getter(42);
-        let num_i32: i32 = getter.get();
-        let num_i64: i64 = getter.get();
-
-        assert_eq!(num_i64, num_i32.into());
+        assert_eq!(MagicNumberGetter::get(), 42);
     }
 }
